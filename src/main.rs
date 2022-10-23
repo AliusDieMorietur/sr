ced7f46bd2315b362ec6c680a6b22f41853c9ca1
AliusDieMorietur <ikamqwerty@gmail.com>
2022-10-23 23:49:17 +0300

    feat: implement multiple addition and substraction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 
+use core::panic;
 use std::io::{stdin,stdout,Write};
 
 const RADIX: u32 = 10;
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum Operation {
+    Plus,
+    Minus, 
+}
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 enum TokenType {
@@ -43,117 +50,145 @@ impl Token {
             value
         }
     }
+
+    fn value_to_number(&self) -> f64 {
+        self.value.clone().unwrap().as_str().parse::<f64>().unwrap()
+    } 
 }
 
 #[derive(Clone, Debug)]
 struct Interpreter {
     pub text: String,
     pub pos: usize,
-    pub current_token: Option<Token>
+    pub current_token: Option<Token>,
+    pub current_char: Option<char>,
 }
 
 impl Interpreter {
     fn new(text: String) -> Interpreter {
         Interpreter {
-            text,
+            text: text.clone(),
             pos: 0,
-            current_token: None
+            current_token: None,
+            current_char: text.clone().chars().nth(0)
         }
     }
 
-    fn get_next_token(&mut self) -> Token {
+
+    fn advance(&mut self) {
+        self.pos += 1;
         if self.pos > self.text.len() - 1 {
-            return Token::new(TokenType::Eof, None)
+            self.current_char = None;
+        } else {
+            self.current_char = self.text.chars().nth(self.pos);
         }
+    }
 
-        let mut chars = self.text.chars();
-        let current_char = chars.nth(self.pos).unwrap();
-
-        if current_char.is_digit(RADIX)  {
-            let mut s = current_char.to_string();
-            let next_char_option = chars.next();
-            if next_char_option.is_none() {
-                self.pos += s.len();
-                return Token::new(
-                    TokenType::Number,
-                    Some(s)
-                );
-            } 
-            let mut next_char = next_char_option.unwrap();
-            while next_char.is_digit(RADIX)  {
-                s.push(next_char);
-                let next_char_option = chars.next();
-                if next_char_option.is_none() {
-                    next_char = 'n';
-                    continue;
-                }
-                next_char = next_char_option.unwrap();
+    fn number(&mut self) -> String {
+        let mut s = String::from("");
+        while let Some(current_char) = self.current_char {
+            if !current_char.is_digit(RADIX) {
+                break;
             }
-            self.pos += s.len();
-            return Token::new(
-                TokenType::Number,
-                Some(s)
-            );
-        };
+            s.push(current_char);
+            self.advance();
+        }
+        return s;
+    }
+        
+    fn get_next_token(&mut self) -> Token {
+        while let Some(current_char) = self.current_char {
+            if current_char.is_digit(RADIX) {
+                return Token::new(TokenType::Number, Some(self.number()));
+            }
+            match current_char {
+                ' ' => {
+                    self.skip_whitespace();
+                    continue;
+                },
+                '+' => {
+                    self.advance();
+                    return Token::new(
+                        TokenType::Plus,
+                        Some(current_char.to_string())
+                    );
+                },
+                '-' => {
+                    self.advance();
+                    return Token::new(
+                        TokenType::Minus,
+                        Some(current_char.to_string())
+                    )
+                },
+                _ => panic!("Can't get next token")
+            }       
+        }
+        return Token::new(TokenType::Eof, None);
 
-        match current_char {
-            ' ' => {
-                self.pos += 1;
-                return self.get_next_token();
-            },
-            '+' => {
-                self.pos += 1;
-                return Token::new(
-                    TokenType::Plus,
-                    Some(current_char.to_string())
-                );
-            },
-            '-' => {
-                self.pos += 1;
-                return Token::new(
-                    TokenType::Minus,
-                    Some(current_char.to_string())
-                )
-            },
-            _ => panic!("Can't get next token")
+    }
+    
+    fn skip_whitespace(&mut self) {
+        if let Some(current_char) = self.current_char {
+            if current_char == ' ' {
+                self.advance();
+            }
+        }
+    }
+
+    fn eat(&mut self, token_type: TokenType)  {
+        if let Some(current_token) = self.current_token.clone() {
+            if current_token.token_type == token_type {
+                self.current_token = Some(self.get_next_token());
+            } else {
+                panic!("Unallowed token type");
+            }
         }
     }
     
-    fn eat(&mut self,)  {
-        self.current_token = Some(self.get_next_token());
-    }
 
     fn expr(&mut self) -> f64 {
         self.current_token = Some(self.get_next_token());
 
-        let left = self.current_token.clone();
-        self.eat();
+        let mut result = self.current_token.clone().unwrap().value_to_number();
+        let mut current_number;
+        let mut current_operation: Option<Operation> = None;
 
-        let op = self.current_token.clone();
-        self.eat();
+        self.eat(TokenType::Number);
 
-        let right = self.current_token.clone();
-        self.eat();
-
-        let op = op.unwrap().value.unwrap();
-        let left = left.unwrap().value.unwrap(); 
-        let right = right.unwrap().value.unwrap();
-
-        match op.as_str() {
-            "+" => {
-                let left = left.as_str().parse::<f64>().unwrap();
-                let right = right.as_str().parse::<f64>().unwrap();
-                return left + right;
-            },
-            "-" => {
-                let left = left.as_str().parse::<f64>().unwrap();
-                let right = right.as_str().parse::<f64>().unwrap();
-                return left - right;
-            },
-            _ => {
-                panic!("unallowed operator")
+        while self.current_token.clone().unwrap().token_type != TokenType::Eof {
+            let token_type = self.current_token.clone().unwrap().token_type;
+            if token_type == TokenType::Number {
+                current_number = self.current_token.clone().unwrap().value_to_number();
+                self.eat(TokenType::Number);
+                match current_operation {
+                    Some(Operation::Minus) => {
+                        result -= current_number;
+                    },
+                    Some(Operation::Plus) => {
+                        result += current_number;
+                    },
+                    None => {
+                        panic!("Unallowed operator")
+                    }
+                }
+                continue;
+            }
+            match token_type {
+                TokenType::Minus => {
+                    current_operation = Some(Operation::Minus);
+                    self.eat(TokenType::Minus);
+                },
+                TokenType::Plus => {
+                    current_operation = Some(Operation::Plus);
+                    self.eat(TokenType::Plus);
+                },
+                _ => {
+                    panic!("Unexpected operator")
+                }
             }
         }
+        result
+       
     }
 }
 
